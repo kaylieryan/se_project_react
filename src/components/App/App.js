d972dc539578cdd894c50e0efd440e50ec70f0a9
kaylieryan <kayliemryan@gmail.com>
2023-08-02 17:08:17 -0700

    start work on header.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,22 @@
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
+    <div>
+      <header className="Header">
+        <div>
+          <div>Logo</div>
+          <div>Date</div>
+        </div>
+        <div>
+          <div>
+            <button type="text">Add New Clothes</button>
+          </div>
+          <div>Name</div>
+          <div>Avatar Logo</div>
+        </div>
       </header>
     </div>
   );
