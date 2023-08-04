24835c678f2edae9289b88f22cad0c8512564687
kaylieryan <kayliemryan@gmail.com>
2023-08-04 08:02:53 -0700

    create header.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,24 @@
-//import logo from "./logo.svg";
+//import logo from "logo.svg";
 import "./App.css";
-
 
 function App() {
   return (
     <div>
-      <header className="Header">
-        <div>
-          <div>Logo</div>
+      <header className="header">
+        <div className="header__logo">
+          <div>
+            <img src="/images/logo.svg" alt="logo" />
+          </div>
           <div>Date</div>
         </div>
-        <div>
+        <div className="header__avatar-logo">
           <div>
             <button type="text">Add New Clothes</button>
           </div>
           <div>Name</div>
-          <div>Avatar Logo</div>
+          <div>
+            <img src="/images/avatar.svg" alt="avatar" />
+          </div>
         </div>
       </header>
     </div>
