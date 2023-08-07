974a3581b27da02788b877e96990827f65c81bb6
kaylieryan <kayliemryan@gmail.com>
2023-08-07 13:44:43 -0700

    style WeatherCard

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -2,16 +2,17 @@ import "./WeatherCard.css";
 import { weatherOptions } from "../../utils/constants.js";
 
 const WeatherCard = ({ day, type, weatherTemp = "" }) => {
-  const imageSource = weatherOptions.filter((option) => {
+  const imageSource = weatherOptions.find((option) => {
     return option.day === day && option.type === type;
   });
 
-  const imageSourceUrl = imageSource[0].url || "";
+  // const imageSourceUrl = imageSource[0].url || "";
+  const imageSourceUrl = imageSource ? weatherOptions[0].url || "" : "";
 
   return (
     <section className="weather" id="weather">
-      <div className="weather_info">{weatherTemp}° F</div>
-      <img src={imageSourceUrl} alt="weather" className="weather_image" />
+      <div className="weather__info">{weatherTemp}° F</div>
+      <img src={imageSourceUrl} alt="weather" className="weather__image" />
     </section>
   );
 };
