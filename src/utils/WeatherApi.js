663cd4a2222b7d631f51ef816b7e22ad86dd9eba
kaylieryan <kayliemryan@gmail.com>
2023-08-07 11:02:44 -0700

    building WeatherApi

---
+++ src/utils/WeatherApi.js
@@ -0,0 +1,26 @@
+import { latitude, longitude, APIkey } from "./constants";
+
+const processServerResponse = (res) => {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Error: ${res.status}`);
+};
+
+export const getForecastWeather = () => {
+  const weatherApi = fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
+  ).then(processServerResponse);
+  return weatherApi;
+};
+
+export const parseWeatherData = (data) => {
+  const main = data.main;
+  const temperature = main && main.temp;
+  return Math.ceil(temperature);
+};
+
+export const parseLocation = (data) => {
+  const city = data.name;
+  return city;
+};
