533a40fbb619ff412077a43625616879a5bd93c1
kaylieryan <kayliemryan@gmail.com>
2023-08-07 11:02:22 -0700

    renaming classes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,33 @@
-import './Header.css'
-import avatarLogo from '../../images/avatar.svg'
-import wtwrLogo from '../../images/logo.svg'
+import "./Header.css";
+import avatarLogo from "../../images/avatar.svg";
+import wtwrLogo from "../../images/logo.svg";
 
-const Header = () => {
-  console.log("Header");
+const currentDate = new Date().toLocaleDateString("default", {
+  month: "long",
+  day: "numeric",
+});
 
+const Header = ({ onCreateModal, city }) => {
   return (
     <header className="header">
       <div className="header__logo">
         <div>
           <img src={wtwrLogo} alt="logo" />
         </div>
-        <div>Date</div>
+        <div>
+          {currentDate}, {city}
+        </div>
       </div>
       <div className="header__avatar-logo">
         <div>
-          <button type="text">Add New Clothes</button>
+          <button
+            type="text"
+            onClick={onCreateModal}
+            className="header__add-clothes-button">
+            + Add clothes
+          </button>
         </div>
-        <div>Name</div>
+        <div>Terrence Tegegne</div>
         <div>
           <img src={avatarLogo} alt="avatar" />
         </div>
