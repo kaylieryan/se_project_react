9916fed6543eae155410e14ce8261ebb25e9d8ef
kaylieryan <kayliemryan@gmail.com>
2023-08-07 13:44:34 -0700

    style Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,14 +4,32 @@ import { defaultClothingItems } from "../../utils/constants.js";
 import "./Main.css";
 
 function Main({ weatherTemp, onSelectCard }) {
+  const getWeatherType = (temperature) => {
+    if (temperature >= 86) {
+      return "hot";
+    } else if (temperature >= 66 && temperature <= 85) {
+      return "warm";
+    } else if (temperature <= 65) {
+      return "cold";
+    }
+  };
+
+  const weatherType = getWeatherType(weatherTemp);
+
+  const filteredCards = defaultClothingItems.filter((item) => {
+    return item.weather.toLowerCase() === weatherType;
+  });
+
   return (
     <main className="main">
       <WeatherCard day={true} type="sunny" weatherTemp={weatherTemp} />
-      <section className="clothing_section" id="card-section">
-        Today is {weatherTemp} / You may want to wear:
-        <div className="clothing_items">
-          {defaultClothingItems.map((item) => (
-            <ItemCard item={item} onSelectCard={onSelectCard} />
+      <section className="clothing" id="clothing-section">
+        <div className="clothing__title">
+          Today is {weatherTemp}° F / You may want to wear:
+        </div>
+        <div className="clothing__items">
+          {filteredCards.map((item, index) => (
+            <ItemCard item={item} onSelectCard={onSelectCard} key={item._id} />
           ))}
         </div>
       </section>
