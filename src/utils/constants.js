f2d0d868f9431daebf85a444bd61d4629d117361
kaylieryan <kayliemryan@gmail.com>
2023-08-08 12:37:58 -0700

    change all weatheOptions to day/

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -38,45 +38,65 @@ export const defaultClothingItems = [
 ];
 
 export const weatherOptions = [
-  { url: require("../images/day/sunny.svg").default, day: true, type: "sunny" },
   {
-    url: require("../images/day/cloudy.svg").default,
+    url: require("../../src/images/day/sunny.svg").default,
+    day: true,
+    type: "sunny",
+  },
+  {
+    url: require("../../src/images/day/cloudy.svg").default,
     day: true,
     type: "cloudy",
   },
-  { url: require("../images/day/rain.svg").default, day: true, type: "rain" },
-  { url: require("../images/day/snow.svg").default, day: true, type: "snow" },
-  { url: require("../images/day/storm.svg").default, day: true, type: "storm" },
-  { url: require("../images/day/fog.svg").default, day: true, type: "fog" },
+  {
+    url: require("../../src/images/day/rain.svg").default,
+    day: true,
+    type: "rain",
+  },
+  {
+    url: require("../../src/images/day/snow.svg").default,
+    day: true,
+    type: "snow",
+  },
+  {
+    url: require("../../src/images/day/storm.svg").default,
+    day: true,
+    type: "storm",
+  },
+  {
+    url: require("../../src/images/day/fog.svg").default,
+    day: true,
+    type: "fog",
+  },
 
   {
-    url: require("../images/night/night_clear.svg").default,
-    night: false,
+    url: require("../../src/images/night/night_clear.svg").default,
+    day: false,
     type: "night-clear",
   },
   {
-    url: require("../images/night/night_cloudy.svg").default,
-    night: false,
+    url: require("../../src/images/night/night_cloudy.svg").default,
+    day: false,
     type: "night-cloudy",
   },
   {
-    url: require("../images/night/night_rain.svg").default,
-    night: false,
+    url: require("../../src/images/night/night_rain.svg").default,
+    day: false,
     type: "night-rain",
   },
   {
-    url: require("../images/night/night_snow.svg").default,
-    night: false,
+    url: require("../../src/images/night/night_snow.svg").default,
+    day: false,
     type: "night-snow",
   },
   {
-    url: require("../images/night/night_storm.svg").default,
-    night: false,
+    url: require("../../src/images/night/night_storm.svg").default,
+    day: false,
     type: "night-storm",
   },
   {
-    url: require("../images/night/night_fog.svg").default,
-    night: false,
+    url: require("../../src/images/night/night_fog.svg").default,
+    day: false,
     type: "night-fog",
   },
 ];
