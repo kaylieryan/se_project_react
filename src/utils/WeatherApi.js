8f25759c1fdd8ae7eef45ec672fd91b356bbe601
kaylieryan <kayliemryan@gmail.com>
2023-08-08 08:37:13 -0700

    change API key

--- src/utils/WeatherApi.js
+++ src/utils/WeatherApi.js
@@ -9,7 +9,7 @@ const processServerResponse = (res) => {
 
 export const getForecastWeather = () => {
   const weatherApi = fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
   ).then(processServerResponse);
   return weatherApi;
 };
