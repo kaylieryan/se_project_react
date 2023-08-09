a482e18fa1351ef1c072c70c244041e2984316c6
kaylieryan <kayliemryan@gmail.com>
2023-08-09 07:35:11 -0700

    rearrange

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -3,14 +3,13 @@ import "./ItemCard.css";
 const ItemCard = ({ item, onSelectCard }) => {
   return (
     <div className="clothing_card">
+      <div className="clothing_card__card_name"> {item.name}</div>
       <img
         src={item.link}
         className="clothing_card__card_image"
         alt="clothing icon"
         onClick={() => onSelectCard(item)}
       />
-
-      <div className="clothing_card__card_name"> {item.name}</div>
     </div>
   );
 };
--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -5,22 +5,20 @@ const ItemModal = ({ selectedCard, onClose }) => {
   return (
     <div className={`item_modal`}>
       <div className="modal__content-item">
+        <button
+          type="button"
+          onClick={onClose}
+          className="modal__close-button-item"
+        ></button>
         <img
           src={selectedCard.link}
           alt={selectedCard.name}
           className="modal__image"
         />
-        <button
-          type="button"
-          onClick={onClose}
-          className="modal__close-button-item"
-        />
         <div className="modal__info-section-container"></div>
         <div className="modal__info">
           <p className="modal__info-name">{selectedCard.name}</p>
-          <p className="modal__info-weather">
-            Weather: {selectedCard.weather}
-          </p>
+          <p className="modal__info-weather">Weather: {selectedCard.weather}</p>
         </div>
       </div>
     </div>
