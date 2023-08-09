6d48f20528e84a59c08d232b5f8669d2aa5e9716
kaylieryan <kayliemryan@gmail.com>
2023-08-09 07:35:29 -0700

    clean up

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -4,18 +4,19 @@ const ModalWithForm = ({
   children,
   buttonText = "Add garment",
   title,
-  modalType,
   onClose,
+  name,
+  onSubmit,
 }) => {
   return (
-    <div className={`modal modal_type_${modalType}`}>
+    <div className={`modal modal_type_${name}`}>
       <div className="modal__content">
         <h3 className="modal__title">{title}</h3>
         <button
           type="button"
           onClick={onClose}
           className="modal__form_close_button"></button>
-        <form className="modal__form">{children}</form>
+        <form onSubmit={onSubmit} className="modal__form">{children}</form>
         <button type="submit" className="modal__submit_button">
           {buttonText}
         </button>
