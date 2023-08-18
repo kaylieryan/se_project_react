4e41e53f4bca3c1e8a193849c271472c941d765f
kaylieryan <kayliemryan@gmail.com>
2023-08-18 07:44:41 -0700

    create ClothingSection

---
+++ src/components/ClothingSection/ClothingSection.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./ClothingSection.css";
+import ItemCard from "../ItemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/constants";
+
+const ClothingSection = ({ onSelectCard }) => {
+  return (
+    <section className="clothingsection">
+      {}
+      <div className="clothingsection__items">
+        {defaultClothingItems.map((item) => (
+          <ItemCard
+            key={item.id}
+            item={item}
+            onSelectCard={onSelectCard}
+            className="clothingsection__item"
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ClothingSection;
+  
