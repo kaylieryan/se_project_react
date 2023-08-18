e5c62eae8136dfc39e07a5dc2927772297a96726
kaylieryan <kayliemryan@gmail.com>
2023-08-18 07:44:15 -0700

    Create Profile section

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,32 @@
 import React from "react";
+import Sidebar from "../Sidebar/Sidebar";
+import ClothingSection from "../ClothingSection/ClothingSection";
+
 import "./Profile.css";
 
+const Profile = ({ onSelectCard }) => {
+  return (
+    <div className="profile">
+      <div className="profile__heading">
+        <div className="profile__heading-user">
+          <img src="" alt="logo" className="profile__avatar-image" />
+          <div className="profile__profile_name">Kaylie Ryan</div>
+        </div>
+        <div className="profile__heading_clothes">
+          <div className="profile__title">Your Items</div>
+          <div className="profile__add_clothes">+ Add new</div>
+        </div>
+      </div>
 
+      <div className="profile__content">
+        <Sidebar />
+        <ClothingSection
+          onSelectCard={onSelectCard}
+          className="clothingsection"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
