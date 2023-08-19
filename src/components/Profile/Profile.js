eb9ffe3d6151e52ab70ea7d11ac716021776b83c
kaylieryan <kayliemryan@gmail.com>
2023-08-19 09:57:28 -0700

    create and style Profile.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,30 +1,17 @@
 import React from "react";
 import Sidebar from "./Sidebar/Sidebar";
 import ClothingSection from "./ClothingSection/ClothingSection";
-
 import "./Profile.css";
 
-const Profile = ({ onSelectCard }) => {
+const Profile = ({ onSelectCard, onCreateModal, clothingItems }) => {
   return (
     <div className="profile">
-      <div className="profile__heading">
-        <div className="profile__heading-user">
-          <img src="" alt="logo" className="profile__avatar-image" />
-          <div className="profile__profile_name">Kaylie Ryan</div>
-        </div>
-        <div className="profile__heading_clothes">
-          <div className="profile__title">Your Items</div>
-          <div className="profile__add_clothes">+ Add new</div>
-        </div>
-      </div>
-
-      <div className="profile__content">
-        <Sidebar />
-        <ClothingSection
-          onSelectCard={onSelectCard}
-          className="clothingsection"
-        />
-      </div>
+      <Sidebar />
+      <ClothingSection
+        onSelectCard={onSelectCard}
+        onCreateModal={onCreateModal}
+        clothingItems={clothingItems}
+      />
     </div>
   );
 };
