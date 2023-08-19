f1e318d6318ab84d7163eccfc4555d5d276ea9ba
kaylieryan <kayliemryan@gmail.com>
2023-08-19 12:03:39 -0700

    set up Api

---
+++ src/utils/Api/Api.js
@@ -0,0 +1,43 @@
+const baseUrl = "http://localhost:3001";
+
+const checkResponse = (response) => {
+  if (response.ok) {
+    return response.json();
+  }
+  return Promise.reject(`Error: ${response.status}`);
+};
+
+// GET Items
+export function getItems() {
+  return fetch(`${baseUrl}/items`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then(checkResponse);
+}
+
+// POST Items
+export function postItems({ name, link, weather }) {
+  console.log(postItems);
+  return fetch(`${baseUrl}/items`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      link,
+      weather,
+    }),
+  }).then(checkResponse);
+}
+
+// DELETE Items
+export function deleteItems(id) {
+  return fetch(`${baseUrl}/items/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then(checkResponse);
+}
