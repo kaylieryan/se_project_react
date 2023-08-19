b96b4b315f9cb628f5cb1d41ba68ebc351ed76f0
kaylieryan <kayliemryan@gmail.com>
2023-08-19 09:58:00 -0700

    Create and style Sidebar

--- src/components/Profile/Sidebar/Sidebar.js
+++ src/components/Profile/Sidebar/Sidebar.js
@@ -1,8 +1,14 @@
 import React from "react";
+import avatar from "../../../images/avatar.svg";
 import "./Sidebar.css";
 
 const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <img src={avatar} alt="avatar" className="sidebar__avatar-image" />
+      <p className="sidebar__avatar-name">Kaylie Ryan</p>
+    </div>
+  );
 };
 
 export default Sidebar;
