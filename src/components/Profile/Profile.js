d15d4ad9240a9bf49bc0fc45c7e470718dac9160
kaylieryan <kayliemryan@gmail.com>
2023-08-19 06:48:56 -0700

    Revert "move and rename files."
    This reverts commit 20a5412d0d2818223e8124e738e793aa7ff5a389.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,6 @@
 import React from "react";
-import Sidebar from "./Sidebar/Sidebar";
-import ClothesSection from "./ClothesSection/ClothesSection";
-import avatar from "../../images/avatar.svg";
+import Sidebar from "../Sidebar/Sidebar";
+import ClothingSection from "../ClothingSection/ClothingSection";
 
 import "./Profile.css";
 
@@ -9,8 +8,8 @@ const Profile = ({ onSelectCard }) => {
   return (
     <div className="profile">
       <div className="profile__heading">
-        <div className="profile__heading_user">
-          <img src={avatar} alt="logo" className="profile__avatar-image" />
+        <div className="profile__heading-user">
+          <img src="" alt="logo" className="profile__avatar-image" />
           <div className="profile__profile_name">Kaylie Ryan</div>
         </div>
         <div className="profile__heading_clothes">
@@ -21,9 +20,9 @@ const Profile = ({ onSelectCard }) => {
 
       <div className="profile__content">
         <Sidebar />
-        <ClothesSection
+        <ClothingSection
           onSelectCard={onSelectCard}
-          className="clothessection"
+          className="clothingsection"
         />
       </div>
     </div>
