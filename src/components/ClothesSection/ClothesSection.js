79fda27127d91926a3d4af2b4b7ecd2d2ad32d54
kaylieryan <kayliemryan@gmail.com>
2023-08-22 10:07:10 -0700

    reconfigure profile.

--- src/components/ClothesSection/ClothesSection.js
+++ src/components/ClothesSection/ClothesSection.js
@@ -3,21 +3,44 @@ import "./ClothesSection.css";
 import ItemCard from "../ItemCard/ItemCard";
 import { defaultClothingItems } from "../../utils/constants";
 
-const ClothesSection = ({ onSelectCard }) => {
+const ClothesSection = ({ onSelectCard, cards, onCreateModal }) => {
   return (
-    <section className="clothingsection">
+    <div className="clothingsection">
+      <div className="clothingsection__title">
+        <h3>Your items</h3>
+        <button
+          className="clothingsection__button"
+          onClick={onCreateModal}
+          type="text">
+          + Add new
+        </button>
+      </div>
       <div className="clothingsection__cards">
-        {defaultClothingItems.map((item) => (
-          <ItemCard
-            item={item}
-            key={item._id}
-            onSelectCard={onSelectCard}
-            className="clothingsection__card"
-          />
+        {cards.map((item) => (
+          <ItemCard item={item} key={item._id} onSelectCard={onSelectCard} />
         ))}
       </div>
-    </section>
+    </div>
   );
 };
 
 export default ClothesSection;
+
+// const ClothesSection = ({ onSelectCard }) => {
+//   return (
+//     <section className="clothingsection">
+//       <div className="clothingsection__cards">
+//         {defaultClothingItems.map((item) => (
+//           <ItemCard
+//             item={item}
+//             key={item._id}
+//             onSelectCard={onSelectCard}
+//             className="clothingsection__card"
+//           />
+//         ))}
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default ClothesSection;
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,32 +1,46 @@
 import React from "react";
 import SideBar from "../SideBar/SideBar";
-import ClothesSection from "../ClothesSection/ClothesSection";
-import avatar from "../../images/avatar.svg";
+//import avatar from "../../images/avatar.svg";
 import "./Profile.css";
+import ClothesSection from "../ClothesSection/ClothesSection";
 
-const Profile = ({ onSelectCard }) => {
+function Profile({ clothingItems, onCreateModal, onSelectCard }) {
   return (
     <div className="profile">
-      <div className="profile__heading">
-        <div className="profile__heading_user">
-          <img src={avatar} alt="logo" className="profile__avatar-image" />
-          <div className="profile__username">Kaylie Ryan</div>
-        </div>
-        <div className="profile__heading_clothes">
-          <div className="profile__title">Your items</div>
-          <div className="profile__add_new">+ Add new</div>
-        </div>
-      </div>
-
-      <div className="profile__content">
-        <SideBar />
-        <ClothesSection
-          onSelectCard={onSelectCard}
-          className="clothingsection"
-        />
-      </div>
+      <SideBar />
+      <ClothesSection
+        clothingItems={clothingItems}
+        onCreateModal={onCreateModal}
+        onSelectCard={onSelectCard}
+      />
     </div>
   );
-};
+}
 
 export default Profile;
+// const Profile = ({ onSelectCard }) => {
+//   return (
+//     <div className="profile">
+//       <div className="profile__heading">
+//         <div className="profile__heading_user">
+//           <img src={avatar} alt="logo" className="profile__avatar-image" />
+//           <div className="profile__username">Kaylie Ryan</div>
+//         </div>
+//         <div className="profile__heading_clothes">
+//           <div className="profile__title">Your items</div>
+//           <div className="profile__add_new">+ Add new</div>
+//         </div>
+//       </div>
+
+//       <div className="profile__content">
+//         <SideBar />
+//         <ClothesSection
+//           onSelectCard={onSelectCard}
+//           className="clothingsection"
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Profile;
