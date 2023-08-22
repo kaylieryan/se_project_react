3faae1fb94f899376e8d76acb4c4cd8b022bcdd7
kaylieryan <kayliemryan@gmail.com>
2023-08-22 10:07:23 -0700

    reconfigure SideBar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./SideBar.css";
+import avatar from "../../images/avatar.svg";
 
-const SideBar = () => {
-  return <div className="sidebar"></div>;
-};
+function SideBar() {
+  return (
+    <div className="sidebar">
+      <img src={avatar} alt="avatar" className="sidebar__avatar-image" />
+      <p className="sidebar__username">Kaylie Ryan</p>
+      </div>
+  );
+}
 
 export default SideBar;
+
+// const SideBar = () => {
+//   return <div className="sidebar"></div>;
+// };
+
+// export default SideBar;
