c65fe8cb542adbd20a715f76dbc2a8663067bc85
kaylieryan <kayliemryan@gmail.com>
2023-08-22 15:11:13 -0700

    work on toggleswitch

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -17,18 +17,18 @@ const ToggleSwitch = () => {
       />
       <span
         className={
-          currentTemperatureUnit === "F"
-            ? "switch__slider switch__slider-C"
-            : "switch__slider switch__slider-F"
+          currentTemperatureUnit === "C"
+            ? "switch__slider switch__slider-F"
+            : "switch__slider switch__slider-C"
         }></span>
       <p
-        className={`switch__temp-F ${
+        className={`switch__temp-C ${
           currentTemperatureUnit === "C" && "switch__active"
         }`}>
         C
       </p>
       <p
-        className={`switch__temp-C ${
+        className={`switch__temp-F ${
           currentTemperatureUnit === "F" && "switch__active"
         }`}>
         F
