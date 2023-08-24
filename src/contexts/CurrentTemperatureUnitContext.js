6490e7510c4e93598f08ec79af2a4bc0954bc808
kaylieryan <kayliemryan@gmail.com>
2023-08-24 06:46:14 -0700

    update contexts

--- src/contexts/CurrentTemperatureUnitContext.js
+++ src/contexts/CurrentTemperatureUnitContext.js
@@ -1,8 +1,6 @@
 import React from "react";
 
-const CurrentTemperatureUnitContext = React.createContext();
-
-const handleToggleSwitchChange = React.createContext({
+const CurrentTemperatureUnitContext = React.createContext({
   currentTemperatureUnit: "",
   handleToggleSwitchChange: () => {},
 });
