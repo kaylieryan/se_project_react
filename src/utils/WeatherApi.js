172a8fdd9275d8578bc79b6503400f6c43e584c8
kaylieryan <kayliemryan@gmail.com>
2024-01-20 08:48:14 -0700

    Remove commented lines.

--- src/utils/WeatherApi.js
+++ src/utils/WeatherApi.js
@@ -19,6 +19,3 @@ export const parseWeatherData = (data) => {
   };
   return weather;
 };
-
-//weather.temperature.F = ` ${Math.round(data.main.temp)}°F`;
-//weather.temperature.C = ` ${Math.round((data.main.temp - 32) * 5/9)}°C`;
