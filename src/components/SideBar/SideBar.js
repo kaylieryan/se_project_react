d0e864a8db0a3335248a1ce877d77813a56fe8e5
kaylieryan <kayliemryan@gmail.com>
2024-02-01 12:53:12 -0700

    sidebar work.

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./SideBar.css";
-import avatar from "../../images/avatar.svg";
+//import avatar from "../../images/avatar.svg";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
-function SideBar() {
+const SideBar = ({ onEditProfileModal, onLogOut }) => {
+  const currentUser = useContext(CurrentUserContext);
+
+  const handleLogoutClick = () => {
+    onLogOut();
+  };
+
   return (
     <div className="sidebar">
-      <img src={avatar} alt="avatar" className="sidebar__avatar-image" />
-      <p className="sidebar__username">Kaylie Ryan</p>
+      <div className="sidebar__avatar-name">
+        <img
+          className="sidebar__avatar-image"
+          src={currentUser.avatar}
+          alt={currentUser.name}></img>
+        <div className="profile__name">{currentUser.name}</div>
       </div>
+      <div className="sidebar__button-container">
+        <button
+          className="sidebar__button"
+          type="button"
+          onClick={onEditProfileModal}>
+          Change Profile Data
+        </button>
+        <button
+          className="sidebar__button"
+          type="button"
+          onClick={handleLogoutClick}>
+          Log Out
+        </button>
+      </div>
+    </div>
   );
-}
+};
 
 export default SideBar;
