9e6a43bd13edb43f6660ee89ceff6432baf69cba
kaylieryan <kayliemryan@gmail.com>
2024-02-01 12:53:46 -0700

    create edit profile modal

---
+++ src/components/EditProfileModal/EditProfileModal.js
@@ -0,0 +1,68 @@
+import React, { useState, useContext } from "react";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+
+const EditProfileModal = ({
+  handleCloseModal,
+  onSubmit,
+  isLoading,
+  isOpen,
+}) => {
+  const currentUser = useContext(CurrentUserContext);
+  const [name, setName] = useState(currentUser.name);
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const [avatar, setAvatar] = useState(currentUser.avatar);
+  const handleAvatarChange = (e) => {
+    setAvatar(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({
+      name,
+      avatar,
+    });
+  };
+
+  const isEnabled = name.length > 0 && avatar.length > 0;
+
+  return (
+    <ModalWithForm
+      buttonText={isLoading ? "Submitting Changes..." : "Submit Changes"}
+      onClose={handleCloseModal}
+      onSubmit={handleSubmit}
+      isOpen={isOpen}
+      isEnabled={isEnabled}>
+      <h2>Change Profile Data</h2>
+      <label className="modal__label">
+        Name
+        <input
+          className="modal__input"
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleNameChange}
+          placeholder="Name"
+          minLength="1"
+          maxLength="30"></input>
+      </label>
+      <label className="modal__label">
+        Avatar URL
+        <input
+          className="modal__input"
+          type="url"
+          name="avatar"
+          value={avatar}
+          onChange={handleAvatarChange}
+          placeholder="Avatar URL"
+          minLength="1"></input>
+      </label>
+    </ModalWithForm>
+  );
+};
+
+export default EditProfileModal;
