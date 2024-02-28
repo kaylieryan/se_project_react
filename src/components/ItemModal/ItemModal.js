10ae1f257b1dbe57380f4f07dd6829055af6dff6
kaylieryan <kayliemryan@gmail.com>
2024-02-28 10:31:07 -0700

    why won't delete button appear?

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -5,39 +5,43 @@ import React from "react";
 
 const ItemModal = ({ selectedCard, onClose, handleDeleteCard }) => {
   const { currentUser } = useContext(CurrentUserContext);
+  console.log(currentUser);
 
-  //const isOwner = selectedCard.owner === currentUser._id;
   const isOwner =
     selectedCard && currentUser && selectedCard.owner === currentUser._id;
-  const itemDeleteButtonClassName = ` ${
-    isOwner ? "item_modal__delete-button" : "item_modal__delete-button_hidden"
+  console.log(isOwner);
+  console.log(selectedCard);
+  console.log(currentUser);
+  const itemModalDeleteButton = ` ${
+    isOwner ? "modal__delete-button_visible" : "modal__delete-button_hidden"
   }`;
 
   return (
-    <div className={`item_modal`}>
+    <div className={`item__modal`}>
       <div className="item_modal__content">
         <button
           type="button"
           onClick={onClose}
-          className="item_modal__close_button"
-        />
+          className="item_modal__close_button"></button>
         <img
           src={selectedCard.imageUrl}
           className="item_modal__item_image"
           alt={selectedCard.name}
         />
         <div className="item_modal__caption">
-          <div className="item_modal__caption_name">{selectedCard.name}</div>
-          <div className="item_modal__caption_weather">
-            Weather: {selectedCard.weather}
+          <div>
+            <div className="item_modal__caption_name">{selectedCard.name}</div>
+            <div className="item_modal__caption_weather">
+              Weather: {selectedCard.weather}
+            </div>
           </div>
+          <button
+            type="button"
+            className={itemModalDeleteButton}
+            onClick={() => handleDeleteCard(selectedCard)}>
+            Delete Item
+          </button>
         </div>
-        <button
-          type="button"
-          className={itemDeleteButtonClassName}
-          onClick={() => handleDeleteCard(selectedCard)}>
-          Delete item
-        </button>
       </div>
     </div>
   );
